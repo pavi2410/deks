5a3992ee304cd97e55692cf4aa72abb0dbc6d24a
Pavitra Golchha <hello@pavi2410.me>
2022-01-15 17:40:42 +0530

    feat: monitor connected devices

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,22 +1,51 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.material.Button
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import com.malinskiy.adam.AndroidDebugBridgeClientFactory
+import com.malinskiy.adam.interactor.StartAdbInteractor
+import com.malinskiy.adam.request.device.AsyncDeviceMonitorRequest
+import com.malinskiy.adam.request.device.Device
+import com.malinskiy.adam.request.device.ListDevicesRequest
+import kotlinx.coroutines.channels.ReceiveChannel
 
 @Composable
 @Preview
 fun App() {
-    var text by remember { mutableStateOf("Hello, World!") }
+    val devices = remember { mutableStateListOf<Device>() }
+
+    LaunchedEffect(Unit) {
+        StartAdbInteractor().execute()
+
+        val adb = AndroidDebugBridgeClientFactory().build()
+
+        devices.addAll(adb.execute(ListDevicesRequest()))
+
+        val deviceEventsChannel: ReceiveChannel<List<Device>> = adb.execute(
+            request = AsyncDeviceMonitorRequest(),
+            scope = this
+        )
+
+        for (currentDeviceList in deviceEventsChannel) {
+            devices.clear()
+            devices.addAll(currentDeviceList)
+        }
+
+    }
 
     MaterialTheme {
-        Button(onClick = {
-            text = "Hello, Desktop!"
-        }) {
-            Text(text)
+        LazyColumn {
+            items(devices) { device ->
+                Text("${device.serial} (${device.state})")
+            }
         }
     }
 }
